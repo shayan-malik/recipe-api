26cec31df5e24458a30701fefb8e5b6d70cedef4
Shayan Malik <shayaaanmalik@gmail.com>
2026-05-11 02:30:48 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from "axios";
+import img from "./images/chef.png"
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [color, setColor] = useState(true);
 
-  const myRecipe = async() => {
-    try{
+  const myRecipe = async () => {
+    try {
       const recipe = await axios.get("https://dummyjson.com/recipes");
       setProducts(recipe.data.recipes);
     }
 
-    catch(error){
+    catch (error) {
       console.log("Error", error)
     }
-    
+
   }
 
   useEffect(() => {
@@ -26,23 +28,48 @@ function App() {
       <div className="main">
         <div className="cards">
           {products.map((eachRecipe) => {
-            return(
+            return (
               <div className="card" key={eachRecipe.id}>
                 <div className="recImage">
                   <img src={eachRecipe.image} alt={eachRecipe.name} />
                 </div>
 
                 <div className="info">
-                <h3>{eachRecipe.name}<span>New</span></h3>
+                  <div className="c-top">
+                  <span>New</span>
+                  <h3>{eachRecipe.name}</h3>
+                  <p className='descr'>{eachRecipe.instructions[0]}</p>
                   <div className="tags">
                     <p>{eachRecipe.tags[0]}</p>
                     <p>{eachRecipe.mealType[0]}</p>
                     <p>{eachRecipe.cookTimeMinutes}mins</p>
-                    </div>
+                  </div>
 
+                  </div>
+
+                  <div className="flex">
                     <div className="btn">
-                      <button>Add to Cart</button>
+                      <div className="diff">
+                        <img src={img} className='rating' alt="" />
+                        <p>{eachRecipe.difficulty}</p>
+                      </div>
+
+
+                      <div className="rat">
+                        <div className="d">
+                          <img src="https://cdn-icons-png.flaticon.com/128/2077/2077502.png" className='rating' alt="" />
+                          <p>{eachRecipe.rating}</p>
+                        </div>
+
+                        <div className="d">
+                          <img src="https://cdn-icons-png.flaticon.com/128/11213/11213197.png" className='rating' alt="" />
+                          <p>{eachRecipe.reviewCount}</p>
+                        </div>
+                      </div>
                     </div>
+                  </div>
+
+
                 </div>
               </div>
             )
